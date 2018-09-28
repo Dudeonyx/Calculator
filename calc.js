3767122663a3096d8bc78ebca0653d7ed1fdab2c
Umeayo Onyekachukwu <umeayo.o.h@gmail.com>
2018-09-28 22:46:28 +0100

    added operator function

--- calc.js
+++ calc.js
@@ -1,12 +1,20 @@
-function add (...args) {
+function add(...args) {
   return args.reduce((a, b) => a + b);
 }
-function substract (...args) {
+function substract(...args) {
   return args.reduce((a, b) => a - b);
 }
-function multiply (...args) {
+function multiply(...args) {
   return args.reduce((a, b) => a * b, 1);
 }
-function divide (...args) {
+function divide(...args) {
   return args.reduce((a, b) => a / b);
+}
+
+function operator(...args) {
+  const newArgs = args.slice(1).map(Number);
+  if (args[0] === '+') return add(...newArgs);
+  if (args[0] === '-') return substract(...newArgs);
+  if (args[0] === '×' || args[0] === '*') return multiply(...newArgs);
+  if (args[0] === '÷' || args[0] === '/') return divide(...newArgs);
 }
