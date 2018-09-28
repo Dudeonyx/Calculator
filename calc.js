93e00f43919a01a1a5e23d696585ff84697e3d7a
Umeayo Onyekachukwu <umeayo.o.h@gmail.com>
2018-09-28 22:45:59 +0100

    init

--- calc.js
+++ calc.js
@@ -1,3 +1,12 @@
-function add([...args]) {
+function add (...args) {
   return args.reduce((a, b) => a + b);
+}
+function substract (...args) {
+  return args.reduce((a, b) => a - b);
+}
+function multiply (...args) {
+  return args.reduce((a, b) => a * b, 1);
+}
+function divide (...args) {
+  return args.reduce((a, b) => a / b);
 }
